1cbc2e7eb9c27a923d763c1110bd7eae3f93b781
Swetha <swethaben111@gmail.com>
2025-01-23 16:21:24 +0530

    frontend updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"; // Import Home.jsx
+import About from "./pages/About"; // Import About.jsx
+import ProjectDetails from "./pages/ProjectDetails"; // Import ProjectDetails.jsx
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects/:id" element={<ProjectDetails />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
